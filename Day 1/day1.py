7f0396e3b7dfe37a161e41ad458ed50f488275ce
Yugansh Dhir <54627277+yugansh-dhir@users.noreply.github.com>
2020-12-06 13:06:59 +0530

    Add files via upload

---
+++ Day 1/day1.py
@@ -0,0 +1,19 @@
+# 1
+print("30 days 30 hour challenge")
+
+Hours = "thirty"
+print(Hours)
+
+Challenge = "I will win"
+print(Challenge[7:10])
+print(Challenge.lower())
+print(Challenge.casefold())
+print(len(Challenge))
+
+a = "30 Days"
+b = "30 Hours"
+print(a+ " "+b)
+
+# 2
+a = "30 days 30 hour challenge"
+print(a[1: ])
---
+++ Day 2/task 2.py
@@ -0,0 +1,20 @@
+#1
+a=b=c=12
+print(a,b,c)
+
+a=a//10
+b=b*50
+c=c+60
+print(a,b,c)
+
+#2
+string="Help"
+print(string)
+print(string.replace("l","G"))
+#string.replace func replaces every "l" in string with "G" without affecting it
+
+#3
+a=100
+b=3.14
+print(float(a))
+print(int(b))
